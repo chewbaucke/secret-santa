8974d6b5606899673725736d2c435d08c72f10b8
Thomas Baucke <thomas.baucke@spinify.com>
2025-11-23 11:25:54 +1100

    Removes redundant env check for test environment

--- jest.setup.js
+++ jest.setup.js
@@ -3,7 +3,6 @@ import '@testing-library/jest-dom'
 
 // Ensure we're using development React for tests
 // This prevents "act(...) is not supported in production builds" error
-if (process.env.NODE_ENV !== 'test') {
-  process.env.NODE_ENV = 'test'
-}
+// Note: NODE_ENV is already set to 'test' by the test script in package.json
+process.env.NODE_ENV = 'test'
 
